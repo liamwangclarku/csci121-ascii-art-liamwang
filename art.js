419f82a4ea8a77585cf2628fdedf2f5cc43c2bd2
catalincv <catalin.veghes@gmail.com>
2022-12-07 01:59:33 -0500

    Update art.js

--- art.js
+++ art.js
@@ -7,4 +7,8 @@ const ART = [
    "filename": "earth.txt",
    "credit_text": "jgs",
    "credit_url": "https://www.asciiart.eu/space/planets"},
+  {"title": "Car",
+   "filename": "car_art.txt",
+   "credit_text": "jgs",
+   "credit_url": "https://www.asciiart.eu/space/planets"},
 ];
